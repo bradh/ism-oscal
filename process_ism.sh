7b027f3aa2433e3631b77f307e2784be1f3481e7
Brad Hards <bradh@frogmouth.net>
2021-12-05 14:01:16 +1100

    update to Dec 2021 ISM release

--- process_ism.sh
+++ process_ism.sh
@@ -1,7 +1,8 @@
 SOURCE=https://www.cyber.gov.au/sites/default/files/
 MONTH_PART=$(date +%Y-%m)
 FILE_DATE_PART=$(date +%B\ %Y)
-ZIP_FILE_NAME="ISM ${FILE_DATE_PART} (${FILE_DATE_PART}).zip"
+# apparently they're better at infosec than spelling...
+ZIP_FILE_NAME="Information Secuirty Manual (${FILE_DATE_PART}).zip"
 DOWNLOADPATH=$SOURCE$MONTH_PART/$ZIP_FILE_NAME
 
 E8_MAPPING="essential-eight-ism-mapping"
@@ -19,7 +20,7 @@ WORK_DIR=$(mktemp -d -t ism-oscal-XXXXXXXXXXXXXXX)
 unzip -q "$ZIP_FILE_NAME" -d $WORK_DIR
 
 java -jar ozcal/xml2oscal/target/xml2oscal-1.0-SNAPSHOT-jar-with-dependencies.jar \
-    "$WORK_DIR/ISM - List of Security Controls ($FILE_DATE_PART).xml" \
+    "$WORK_DIR/List of Security Controls ($FILE_DATE_PART).xml" \
     "Australian Government Information Security Manual ($FILE_DATE_PART)" \
     $MONTH_PART \
     "$E8_MAPPING"
